f80dad68b9f0ec7726575105d9c5e243c69e782e
Muhammad Zaini <163093916+muhammadzaini213@users.noreply.github.com>
2024-09-05 14:09:19 +0700

    Update nomor_2.py

--- laprak_alpro1/nomor_2.py
+++ laprak_alpro1/nomor_2.py
@@ -3,3 +3,10 @@ celcius = float(input("Suhu dalam Celcius('C): "))
 fahrenheit = (celcius/5*9)+32
 
 print("Suhu dalam Fahrenheit('F):", fahrenheit)
+
+
+# konveter suhu
+# 1.Input berguna untuk memasukkan nilai ke variabel celcius, float akan memastikan bahwa input yang dimasukkan adalah angka dan
+#   memastikan pengguna dapat memasukkan bilangan bulat maupun bilangan dengan angka dibelakang koma (desimal)
+# 2.Sistem akan memasukkan input dari pengguna kedalam rumus konversi
+# 3.Program akan mencetak hasil perhitungan
