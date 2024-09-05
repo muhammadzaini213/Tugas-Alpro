9733c9308121a7fbf4de998a557a97d12aa115dd
muhammadzaini213 <oniichanterritory@gmail.com>
2024-09-05 08:02:41 +0700

    selesain latihan

--- Nyobain/perulangan_x.py
+++ Nyobain/perulangan_x.py
@@ -4,6 +4,7 @@ x = int(input("Masukkkan nilai x: "))
 
 print("\n--------------------------Hasil--------------------------\n")
 
+# Menggunakan while loop untuk mleakukan operasi
 while(x < 10):
     print(f"x = {x}")
     x = x + 1
