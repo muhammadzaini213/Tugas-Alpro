95f83f4d4b784b9ca9860639090e18c03ff66ea5
Muhammad Zaini <163093916+muhammadzaini213@users.noreply.github.com>
2024-09-05 14:09:42 +0700

    Update nomor_3.py

--- laprak_alpro1/nomor_3.py
+++ laprak_alpro1/nomor_3.py
@@ -7,3 +7,8 @@ gaji_seminggu = upah_perjam * jam_perhari * hari_seminggu
 
 print("Gaji pegawai perminggu adalah:", gaji_seminggu)
 
+# gaji pegawai
+# 1.Input akan digunakan untuk memasukkan gaji perjam pegawai yang diinginkan pengguna serta akan dipastikan berubah menjadi int atau bilangan bulat
+# 2.Program menghitung gaji perhari menggunakan rumus
+# 3.Program menghitung gaji perminggun menggunakan rumus
+# 4.Program mencetak hasil perhitungan
