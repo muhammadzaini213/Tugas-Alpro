2d0c7184d7c1b173c9c0d95c88971f9beb77bbe4
muhammadzaini213 <oniichanterritory@gmail.com>
2024-09-10 09:27:59 +0800

    ,ain

---
+++ Catatan/Alpro 2/delay.py
@@ -0,0 +1,8 @@
+import time
+
+i = 1
+
+while(i != 10000):
+    print(i)
+    i += 1
+    time.sleep(0.3)
--- laprak_alpro2/protein.py
+++ laprak_alpro2/protein.py
@@ -1,1 +1,5 @@
-print(f"\033[34m-----------------------------Selamat datang di rogram penerjemah protein-----------------------------")
+print(f"\033[34m-----------------------------Selamat datang di program penerjemah protein-----------------------------\n")
+
+kodon = input
+if(kodon == "AUG"):
+    print()
